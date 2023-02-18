c990659c2f7e333678e0a3ca427f9dece50a5338
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-18 02:53:26 -0500

    feat: :tada: A site made with nim and prologue
    Meet the knights of nim, a site for nimble people and go getters. Made with nim and go.

---
+++ app.nim
@@ -0,0 +1,17 @@
+import prologue
+
+import ./urls
+
+let
+  env = loadPrologueEnv(".env")
+  settings = newSettings(appName = env.getOrDefault("appName", "Prologue"),
+                         debug = env.getOrDefault("debug", true),
+                         port = Port(env.getOrDefault("port", 8080)),
+                         secretKey = env.getOrDefault("secretKey", "")
+    )
+
+
+var app = newApp(settings = settings)
+# Be careful with the routes.
+app.addRoute(urls.urlPatterns, "")
+app.run()
