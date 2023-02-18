7253f11d0a60f5a94255d8417c70685879cc0588
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-18 18:57:13 -0500

    organise src and add init .nimble

--- multibuild/main.go
+++ multibuild/main.go
@@ -16,8 +16,31 @@ func main() {
 func build(ctx context.Context) error {
 	fmt.Println("Building with Dagger")
 
-	// initialize Dagger client
-	client, err := dagger.Connect(ctx)
+	    // initialize Dagger client
+		client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+		if err != nil {
+			return err
+		}
+		defer client.Close()
+	
+		// get reference to the local project
+		src := client.Host().Directory(".")
+	
+		// get `golang` image
+		golang := client.Container().From("golang:latest")
+	
+		// mount cloned repository into `golang` image
+		golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
+	
+		// define the application build command
+		path := "build/"
+		golang = golang.WithExec([]string{"go", "build", "-o", path})
+	
+		// get reference to build output directory in container
+		output := golang.Directory(path)
+	
+		// write contents of container build/ directory to the host
+		_, err = output.Export(ctx, path)
 	if err != nil {
 		return err
 	}
