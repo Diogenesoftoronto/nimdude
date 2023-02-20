adb09d535e84adf2dea7981e7b15a1a0474ed214
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-20 05:34:44 -0500

    add text comments to navigation

--- frontend/components/navigation.nim
+++ frontend/components/navigation.nim
@@ -1,26 +1,27 @@
 import karax / [karax, karaxdsl, vdom]
-
+# import strutils
+# type Routes = enum index, about, contact, projects, stats, articles
 var Routes = ["index", "about", "contact", "projects", "stats", "articles"]
 
 proc makeNavigation*(data: RouterData): VNode =
     result = buildHtml(tdiv):
+        # var currentroute = $data.hashPart
+        # currentroute = currentroute[1..currentroute.len]
         for route in Routes:
-            a(class="nav-link", href= "#/"&route):
-                span(class= "nav-link-inner-text"):
-                    text route
-            # let
-            #     route = routes.route
-            #     name = routes.name
-            #     active = data.route == route
-            # tdiv:
-            #     class = "nav-item"
-            #     if active:
-            #         class = "nav-item active"
-            #     tdiv:
-            #         class = "nav-link"
-            #         tspan:
-            #             class = "nav-link-inner-text"
-            #             text = name
-            #         onclick = proc (e: Event) =
-            #             e.preventDefault()
-            #             data.router.push(route)
+            # var style: string
+            # if currentroute == route:
+            #     style = "active"
+            # else:
+            #     style = ""
+            a(class="nav-link", href="#/"&route):
+                span(class="nav-item"):
+                        text route
+# import karax / [karax, karaxdsl, vdom]
+
+
+# proc makeNavigation*(data: RouterData): VNode =
+#     result = buildHtml(tdiv):
+#         for route in Routes:
+#             a(class="nav-link", href= "#/"&route):
+#                 span(class= "nav-link-inner-text"):
+#                     text route
