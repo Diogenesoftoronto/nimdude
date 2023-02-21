0b138ce1743689c594d8ff3332886e3342b29095
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-21 18:32:51 -0500

    style and remove comments!

--- frontend/components/navigation.nim
+++ frontend/components/navigation.nim
@@ -1,27 +1,17 @@
 import karax / [karax, karaxdsl, vdom]
-# import strutils
-# type Routes = enum index, about, contact, projects, stats, articles
-var Routes = ["index", "about", "contact", "projects", "stats", "articles"]
+import strutils
+
+var Routes = ["about", "contact", "projects", "stats", "articles"]
 
 proc makeNavigation*(data: RouterData): VNode =
-    result = buildHtml(tdiv):
-        # var currentroute = $data.hashPart
-        # currentroute = currentroute[1..currentroute.len]
-        for route in Routes:
-            # var style: string
-            # if currentroute == route:
-            #     style = "active"
-            # else:
-            #     style = ""
-            a(class="nav-link", href="#/"&route):
-                span(class="nav-item"):
-                        text route
-# import karax / [karax, karaxdsl, vdom]
-
-
-# proc makeNavigation*(data: RouterData): VNode =
-#     result = buildHtml(tdiv):
-#         for route in Routes:
-#             a(class="nav-link", href= "#/"&route):
-#                 span(class= "nav-link-inner-text"):
-#                     text route
+    result = buildHtml(nav(class="container-fluid")):
+            ul:
+                li:
+                    a(class="primary", href="#/"):
+                            strong(class="lg-text"):
+                                text "Nimdude"
+            ul:
+                for route in Routes:
+                        li:
+                            a(class="secondary", href="#/"&route):
+                                text route.capitalizeAscii
