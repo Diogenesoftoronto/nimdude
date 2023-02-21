90e06c56a962b76885814abb217be45d40f5dafd
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-02-20 23:44:32 -0500

    add nimdude package

--- build.go
+++ build.go
@@ -1,4 +1,4 @@
-package main
+package nimdude
 
 import (
 	"fmt"
@@ -10,13 +10,21 @@ func Build() {
 	// Set the working directory
 	// os.Chdir("/")
 
-	// Build the application
+	// Install dependencies
+	if err := exec.Command("nimble", "install").Run(); err != nil {
+		panic(fmt.Errorf("error installing dependencies: %v", err))
+	}
 
-	if err := exec.Command("go", "build", "-o", "bin").Run(); err != nil {
+	if err := exec.Command("go", "mod", "tidy").Run(); err != nil {
+		panic(fmt.Errorf("error installing dependencies: %v", err))
+	}
+
+	// Build the application
+	if err := exec.Command("nim", "js", "-o:./public/js/app.js", "./frontend/app.nim").Run(); err != nil {
 		panic(fmt.Errorf("error building application: %v", err))
 	}
 
-	if err := exec.Command("nim", "js", "-o", "app").Run(); err != nil {
+	if err := exec.Command("go", "build", "-o", "build").Run(); err != nil {
 		panic(fmt.Errorf("error building application: %v", err))
 	}
 
@@ -29,11 +37,12 @@ func Build() {
 
 	// Write the contents of the Dockerfile
 	file.WriteString("FROM golang:1.18\n")
-	file.WriteString("COPY app /app\n")
-	file.WriteString("CMD [\"/app\"]\n")
+	file.WriteString("FROM nimlang:nim\n")
+	file.WriteString("COPY build /build\n")
+	file.WriteString("CMD [\"/build\"]\n")
 
 	// Build the Docker image
-	cmd := exec.Command("docker", "build", "-t", "myapp:latest", ".")
+	cmd := exec.Command("docker", "build", "-t", "nimdude:latest", ".")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err = cmd.Run()
