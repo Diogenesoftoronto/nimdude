b6808f71739052ee095350d3c5154139fdac25f4
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-04 20:42:06 -0500

    remove unused comments

--- frontend/components/article.nim
+++ frontend/components/article.nim
@@ -1,26 +1,7 @@
 import std/options
-# import std/strutils
-
 import karax/[vdom]
-# import shell
-# generate an article using the hastyscribe to take articles written in markdown and convert them to html and then into karax code.
-# const articleDir = ".."/"wasmdude"/"articles"
-# const mdExt = ".md"
 
-# proc compileArticles*(dir, ext: string): bool =
-#   try:
-#     for dir, file in walkDir(dir):
-#       if file.endsWith(ext):
-#         let article = file[0 ..< file.len - mdExt.len]
-#         shell:
-#             pwd
-#             cd ($dir)
-#             hastyscribe --fragment ($article)
-#       return true
-#   except OSError, IOError:
-#     return false
 
-# when compileArticles(articleDir, mdExt):
 proc Article*(article: string): Option[VNode] =
   try:
     result = article.verbatim.some
