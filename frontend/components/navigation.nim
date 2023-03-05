c0abc401dce092e166a24b562021c95eba24dc29
Diogenesoftoronto <diogenesoft@protonmail.com>
2023-03-04 20:42:44 -0500

    hamburger menu!!

--- frontend/components/navigation.nim
+++ frontend/components/navigation.nim
@@ -1,17 +1,32 @@
 import karax / [karax, karaxdsl, vdom]
 import std/strutils
-
+from sugar import `=>`
 import ../constants
+import std/options
 
-proc makeNavigation*(data: RouterData): VNode =
+
+proc hamMenu*(show: int): Option[VNode] =
+    if show == 1:
+        var menu = buildHtml(aside(class="container-fluid")):
+                    nav:
+                        ul:
+                            for route in Routes:
+                                li:
+                                    a(class="secondary", href="#/"&route):
+                                        text route.capitalizeAscii
+        return some(menu)
+    return none(VNode)
+
+proc mobileNavigation*(s: ptr int): VNode =
     result = buildHtml(nav(class="container-fluid")):
-            ul:
-                li:
-                    a(class="primary", href="#/"):
-                            strong(class="lg-text"):
-                                text "Nimdude"
-            ul:
-                for route in Routes:
-                        li:
-                            a(class="secondary", href="#/"&route):
-                                text route.capitalizeAscii
+        ul:
+            li:
+                button(onclick = () => (s[] *= -1)):
+                    let image = "static/images/hamburger.svg"
+                    img(src = image, class = "footer-img"): discard
+        ul:
+            li:
+                a(class="primary", href="/"):
+                        strong(class="lg-text"):
+                            text "Nimdude"
+
